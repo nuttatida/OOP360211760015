445e47c3451707fa4b8af29ad1a14c2c5ec74d5c
nuttatida <nuttatida_saw@outlook.com>
2018-05-15 16:44:33 +0700

    lab8

---
+++ src/lab8/Pet.java
@@ -0,0 +1,25 @@
+package lab8;
+
+public class Pet {
+    private String name;
+    private int ags;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAgs() {
+        return ags;
+    }
+
+    public void setAgs(int ags) {
+        this.ags = ags;
+    }
+}
+
+
+
---
+++ src/lab8/petApp.java
@@ -0,0 +1,23 @@
+package lab8;
+
+import lab9.Cow;
+
+public class PetApp {
+    public static void main(String[] args) {
+        Dog dog =new Dog("mochi",3,"chihuahua");
+        System.out.println(dog.toString());
+        dog.makeNoise();
+        dog.myPet();
+
+        Cat cat = new Cat("Giant",2,"persian");
+        System.out.println(cat.toString());
+        cat.makeNoise();
+        cat.myPet();
+
+        Cow cow = new Cow("Namjai",5);
+        System.out.println(cow.toString());
+        cow.makeNoise();
+        cow.myPet();
+
+    }//main
+}//class
