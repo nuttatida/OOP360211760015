2623b150d51c0ead7d5d43cc66e57cc92c8394a3
Nuttatida <nuttatida_saw@outlook.com>
2018-05-16 10:59:35 +0700

    carapp

--- src/lab8/carApp.java
+++ src/lab8/carApp.java
@@ -1,7 +1,11 @@
 package lab8;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class carApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Engine e = new Engine();
         e.setEngSize(2500);
         e.setHorsePower(150);
@@ -15,7 +19,22 @@ public class carApp {
 
         System.out.println(c.toString());
 
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        System.out.print("Enter car color: ");
+        String color = reader.readLine();
+        System.out.print("Enter car brand: ");
+        String brand = reader.readLine();
+        System.out.print("Enter car ID: ");
+        String id = reader.readLine();
+        System.out.print("Enter engine size: ");
+        int size = Integer.parseInt(reader.readLine());
+        System.out.print("Enter horse power: ");
+        int hpower = Integer.parseInt(reader.readLine());
+
+        Engine myEng = new Engine(size, hpower);
+        Car myCar = new Car(color, brand, id, myEng);
+        System.out.println(myCar.toString());
 
 
 
